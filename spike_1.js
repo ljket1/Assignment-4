3c791817985ffb25385dc49d1d60187848f72856
ljket1 <ljket1@student.monash.edu>
2017-04-27 16:52:04 +1000

    Add spikes

---
+++ spike_1.js
@@ -0,0 +1,16 @@
+var moreCodeTable = [{
+  letter: 'a',
+  code: '1001'
+}, {
+  letter: 'b',
+  code: '0011'
+}, {
+  letter: 'c',
+  code: '0101'
+}, {
+  letter: 'd',
+  code: '1110'
+}, {
+  letter: 'e',
+  code: '1111'
+}];
---
+++ spike_2.js
@@ -0,0 +1,16 @@
+var morseCodeTableArray = [{
+  letter: 'a',
+  code: ['1', '0', '0', '1']
+}, {
+  letter: 'b',
+  code: ['0', '0', '1', '1']
+}, {
+  letter: 'c',
+  code: ['0', '1', '0', '1']
+}, {
+  letter: 'd',
+  code: ['1', '1', '1', '0']
+}, {
+  letter: 'e',
+  code: ['1', '1', '1', '1']
+}];
